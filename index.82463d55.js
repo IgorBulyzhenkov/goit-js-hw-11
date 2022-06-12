da1be0fd663ca65744473e95e26926faaca84bd9
IgorBulyzhenkov <98158893+IgorBulyzhenkov@users.noreply.github.com>
2022-06-12 17:18:36 +0000

    Deploying to gh-pages from @ IgorBulyzhenkov/goit-js-hw-11@d22877a66edb8414ddcfc2efb691697b5a2c5705 🚀

---
+++ index.82463d55.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.elements.searchQuery.value);const t=e.target.elements.searchQuery.value;console.log(t)}));
+//# sourceMappingURL=index.82463d55.js.map
